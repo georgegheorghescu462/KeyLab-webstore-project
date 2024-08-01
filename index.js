8ac6d13de1cc9ca262bda15745b160d5ad3e72db
georgegheorghescu462 <gheorghescug462@gmail.com>
2024-08-01 20:30:42 +0300

    feat: cart page functional

--- index.js
+++ index.js
@@ -9,4 +9,29 @@ async function displayAllProducts() {
 	const products = await getAllProducts();
 
 	mainContainer.innerHTML = products.map(mapProductToCard).join('');
+
+	const addToCartButtons = document.querySelectorAll('.add-to-cart');
+
+	addToCartButtons.forEach((button) => {
+		button.addEventListener('click', () => {
+			const productId = button.getAttribute('data-id');
+			const name = button.getAttribute('data-name');
+			const price = button.getAttribute('data-price');
+			const imageUrl = button.getAttribute('data-image');
+
+			let cart = JSON.parse(localStorage.getItem('cart')) || {};
+			if (cart[productId]) {
+				cart[productId].quantity++;
+			} else {
+				cart[productId] = {
+					quantity: 1,
+					name: name,
+					price: price,
+					imageUrl: imageUrl,
+				};
+			}
+
+			localStorage.setItem('cart', JSON.stringify(cart));
+		});
+	});
 }
---
+++ js/cart.js
@@ -0,0 +1,59 @@
+import { getProductById } from '../api/products.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+	const cart = JSON.parse(localStorage.getItem('cart'));
+	const cartItemsContainer = document.querySelector('.cart-items');
+	const cartTotalContainer = document.querySelector('.cart-total');
+
+	function updateCart() {
+		cartItemsContainer.innerHTML = '';
+		let total = 0;
+
+		for (let id in cart) {
+			// const product = await getProductById(id);
+			// product.quantity = cart[id].quantity;
+			const product = cart[id];
+
+			const productCard = document.createElement('div');
+			productCard.className = 'flex justify-between items-center w-700';
+			const disabled = product.quantity === 1 ? 'disabled' : '';
+			productCard.innerHTML = `
+			<img width="50px" src="${product.imageUrl}"/>
+            <div class="flex gap-20 w-400 h-40 justify-between items-center">
+				<span>${product.name}</span>				
+            	<div class="flex gap-10">
+              		<button ${disabled} data-id="${id}" class="decrease">-</button>
+               		<span>${product.quantity}</span>
+               		<button data-id="${id}" class="increase">+</button>
+           		</div>
+			</div>
+			<span>$${product.price * product.quantity}</span>
+			<button data-id="${id}" class="delete">X</button>
+            `;
+			total += product.price * product.quantity;
+			cartItemsContainer.appendChild(productCard);
+		}
+		cartTotalContainer.innerHTML =
+			total === 0 ? 'Empty cart' : `Total: $${total}`;
+	}
+
+	cartItemsContainer.addEventListener('click', (e) => {
+		if (e.target.classList.contains('increase')) {
+			const id = e.target.getAttribute('data-id');
+			cart[id].quantity++;
+		} else if (e.target.classList.contains('decrease')) {
+			const id = e.target.getAttribute('data-id');
+			cart[id].quantity--;
+			if (cart[id].quantity <= 0) {
+				delete cart[id];
+			}
+		} else if (e.target.classList.contains('delete')) {
+			const id = e.target.getAttribute('data-id');
+			delete cart[id];
+		}
+		localStorage.setItem('cart', JSON.stringify(cart));
+		updateCart();
+	});
+
+	updateCart();
+});
